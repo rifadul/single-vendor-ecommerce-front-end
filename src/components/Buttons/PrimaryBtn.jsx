adf6fe1a97782f75d5dfa64b247733405c612795
Siam Khushnobish <siam@zaagsys.com>
2024-07-07 17:41:20 +0600

    Home page completed

--- src/components/Buttons/PrimaryBtn.jsx
+++ src/components/Buttons/PrimaryBtn.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 const PrimaryBtn = ({ label, className, ...rest }) => {
-    return (
-        <button className={`${className}`} {...rest}>
-            {label}
-        </button>
-    );
+  return (
+    <button
+      className={`bg-magenta-600 py-3 px-9 text-white font-semibold text-base ${className}`}
+      {...rest}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default PrimaryBtn;
