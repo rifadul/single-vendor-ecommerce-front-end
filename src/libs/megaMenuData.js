16c73dfeca010ea8a6b2ca39061b8c26692dffd1
Siam Khushnobish <siam@zaagsys.com>
2024-07-07 17:56:57 +0600

    Mega Menu UI completed

---
+++ src/libs/megaMenuData.js
@@ -0,0 +1,614 @@
+export const megaMenuData = [
+  {
+    _id: 1,
+    title: "Clothing",
+    child: [
+      {
+        _id: 1,
+        title: "Women's Collection",
+        child: [
+          {
+            _id: 1,
+            title: "New arrivals",
+            child: [],
+          },
+          {
+            _id: 2,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 3,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 4,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 5,
+            title: "Jacket & Sweater",
+            child: [],
+          },
+          {
+            _id: 6,
+            title: "Kurta",
+            child: [],
+          },
+          {
+            _id: 7,
+            title: "Shawls",
+            child: [],
+          },
+          {
+            _id: 8,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 9,
+            title: "Fatua",
+            child: [],
+          },
+          {
+            _id: 10,
+            title: "New Arrivals",
+            child: [],
+          },
+          {
+            _id: 11,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 12,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+        ],
+      },
+      {
+        _id: 2,
+        title: "Men's Collection",
+        child: [
+          {
+            _id: 1,
+            title: "New arrivals",
+            child: [],
+          },
+          {
+            _id: 2,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 3,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 4,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 5,
+            title: "Jacket & Sweater",
+            child: [],
+          },
+          {
+            _id: 6,
+            title: "Kurta",
+            child: [],
+          },
+          {
+            _id: 7,
+            title: "Shawls",
+            child: [],
+          },
+          {
+            _id: 8,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 9,
+            title: "Fatua",
+            child: [],
+          },
+          {
+            _id: 10,
+            title: "New Arrivals",
+            child: [],
+          },
+          {
+            _id: 11,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 12,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+        ],
+      },
+      {
+        _id: 3,
+        title: "Kid's Collection",
+        child: [
+          {
+            _id: 1,
+            title: "New arrivals",
+            child: [],
+          },
+          {
+            _id: 2,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 3,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 4,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 5,
+            title: "Jacket & Sweater",
+            child: [],
+          },
+          {
+            _id: 6,
+            title: "Kurta",
+            child: [],
+          },
+          {
+            _id: 7,
+            title: "Shawls",
+            child: [],
+          },
+          {
+            _id: 8,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 9,
+            title: "Fatua",
+            child: [],
+          },
+          {
+            _id: 10,
+            title: "New Arrivals",
+            child: [],
+          },
+          {
+            _id: 11,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 12,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id: 2,
+    title: "Jewellery",
+    child: [
+      {
+        _id: 1,
+        title: "Women's Collection",
+        child: [
+          {
+            _id: 1,
+            title: "New arrivals",
+            child: [],
+          },
+          {
+            _id: 2,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 3,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 4,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 5,
+            title: "Jacket & Sweater",
+            child: [],
+          },
+          {
+            _id: 6,
+            title: "Kurta",
+            child: [],
+          },
+          {
+            _id: 7,
+            title: "Shawls",
+            child: [],
+          },
+          {
+            _id: 8,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 9,
+            title: "Fatua",
+            child: [],
+          },
+          {
+            _id: 10,
+            title: "New Arrivals",
+            child: [],
+          },
+          {
+            _id: 11,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 12,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+        ],
+      },
+      {
+        _id: 2,
+        title: "Men's Collection",
+        child: [
+          {
+            _id: 1,
+            title: "New arrivals",
+            child: [],
+          },
+          {
+            _id: 2,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 3,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 4,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 5,
+            title: "Jacket & Sweater",
+            child: [],
+          },
+          {
+            _id: 6,
+            title: "Kurta",
+            child: [],
+          },
+          {
+            _id: 7,
+            title: "Shawls",
+            child: [],
+          },
+          {
+            _id: 8,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 9,
+            title: "Fatua",
+            child: [],
+          },
+          {
+            _id: 10,
+            title: "New Arrivals",
+            child: [],
+          },
+          {
+            _id: 11,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 12,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+        ],
+      },
+      {
+        _id: 3,
+        title: "Kid's Collection",
+        child: [
+          {
+            _id: 1,
+            title: "New arrivals",
+            child: [],
+          },
+          {
+            _id: 2,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 3,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 4,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 5,
+            title: "Jacket & Sweater",
+            child: [],
+          },
+          {
+            _id: 6,
+            title: "Kurta",
+            child: [],
+          },
+          {
+            _id: 7,
+            title: "Shawls",
+            child: [],
+          },
+          {
+            _id: 8,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 9,
+            title: "Fatua",
+            child: [],
+          },
+          {
+            _id: 10,
+            title: "New Arrivals",
+            child: [],
+          },
+          {
+            _id: 11,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 12,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id: 3,
+    title: "Dry Foods",
+    child: [
+      {
+        _id: 1,
+        title: "Women's Collection",
+        child: [
+          {
+            _id: 1,
+            title: "New arrivals",
+            child: [],
+          },
+          {
+            _id: 2,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 3,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 4,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 5,
+            title: "Jacket & Sweater",
+            child: [],
+          },
+          {
+            _id: 6,
+            title: "Kurta",
+            child: [],
+          },
+          {
+            _id: 7,
+            title: "Shawls",
+            child: [],
+          },
+          {
+            _id: 8,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 9,
+            title: "Fatua",
+            child: [],
+          },
+          {
+            _id: 10,
+            title: "New Arrivals",
+            child: [],
+          },
+          {
+            _id: 11,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 12,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+        ],
+      },
+      {
+        _id: 2,
+        title: "Men's Collection",
+        child: [
+          {
+            _id: 1,
+            title: "New arrivals",
+            child: [],
+          },
+          {
+            _id: 2,
+            title: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 3,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 4,
+            title: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 5,
+            title: "Jacket & Sweater",
+            child: [],
+          },
+          {
+            _id: 6,
+            title: "Kurta",
+            child: [],
+          },
+          {
+            _id: 7,
+            title: "Shawls",
+            child: [],
+          },
+          {
+            _id: 8,
+            title: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 9,
+            title: "Fatua",
+            child: [],
+          },
+          {
+            _id: 10,
+            title: "New Arrivals",
+            child: [],
+          },
+          {
+            _id: 11,
+            childTitle: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 12,
+            childTitle: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+        ],
+      },
+      {
+        _id: 3,
+        title: "Kid's Collection",
+        child: [
+          {
+            _id: 1,
+            childTitle: "New arrivals",
+            child: [],
+          },
+          {
+            _id: 2,
+            childTitle: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 3,
+            childTitle: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 4,
+            childTitle: "Kamiz",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 5,
+            childTitle: "Jacket & Sweater",
+            child: [],
+          },
+          {
+            _id: 6,
+            childTitle: "Kurta",
+            child: [],
+          },
+          {
+            _id: 7,
+            childTitle: "Shawls",
+            child: [],
+          },
+          {
+            _id: 8,
+            childTitle: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+          {
+            _id: 9,
+            childTitle: "Fatua",
+            child: [],
+          },
+          {
+            _id: 10,
+            childTitle: "New Arrivals",
+            child: [],
+          },
+          {
+            _id: 11,
+            childTitle: "Saree",
+            child: ["Cotton", "Muslin", "Silk", "Katan", "Jamdani"],
+          },
+          {
+            _id: 12,
+            childTitle: "Shirts",
+            child: ["Classic", "Exclusive"],
+          },
+        ],
+      },
+    ],
+  },
+];
