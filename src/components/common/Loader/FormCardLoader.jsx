82a04f8069785f1a08a51c09ca05449c059e6b75
Siam Khushnobish <siam@zaagsys.com>
2024-07-08 15:20:14 +0600

    change class to class name

--- src/components/common/Loader/FormCardLoader.jsx
+++ src/components/common/Loader/FormCardLoader.jsx
@@ -2,31 +2,31 @@ import React from "react";
 
 function FormCardLoader() {
     return (
-        <div class="min-h-screen flex items-center justify-center animate-pulse">
-            <div class="bg-white p-12 rounded-lg shadow-light-gray w-full max-w-lg flex flex-col gap-12">
-                <div class="h-6 bg-gray-200 rounded w-1/2 mx-auto"></div>
-                <div class="h-6 bg-gray-200 rounded w-1/2 mx-auto"></div>
-                <div class="h-44 bg-gray-200 rounded w-full mx-auto"></div>
-                <div class="space-y-9">
-                    <div class="space-y-3">
-                        <div class="h-6 bg-gray-200 rounded w-1/2"></div>
-                        <div class="h-4 bg-gray-200 rounded w-full mx-auto"></div>
+        <div className="min-h-screen flex items-center justify-center animate-pulse">
+            <div className="bg-white p-12 rounded-lg shadow-light-gray w-full max-w-lg flex flex-col gap-12">
+                <div className="h-6 bg-gray-200 rounded w-1/2 mx-auto"></div>
+                <div className="h-6 bg-gray-200 rounded w-1/2 mx-auto"></div>
+                <div className="h-44 bg-gray-200 rounded w-full mx-auto"></div>
+                <div className="space-y-9">
+                    <div className="space-y-3">
+                        <div className="h-6 bg-gray-200 rounded w-1/2"></div>
+                        <div className="h-4 bg-gray-200 rounded w-full mx-auto"></div>
                     </div>
 
-                    <div class="flex flex-col gap-4">
-                        <div class="h-10 bg-gray-200 rounded w-full mx-auto"></div>
-                        <div class="h-10 bg-gray-200 rounded w-full mx-auto"></div>
+                    <div className="flex flex-col gap-4">
+                        <div className="h-10 bg-gray-200 rounded w-full mx-auto"></div>
+                        <div className="h-10 bg-gray-200 rounded w-full mx-auto"></div>
 
-                        <div class="flex justify-between items-center mb-4">
-                            <div class="w-1/2 h-4 bg-gray-200 rounded"></div>
-                            <div class="w-1/2 h-4 bg-gray-200 rounded"></div>
+                        <div className="flex justify-between items-center mb-4">
+                            <div className="w-1/2 h-4 bg-gray-200 rounded"></div>
+                            <div className="w-1/2 h-4 bg-gray-200 rounded"></div>
                         </div>
 
-                        <div class="h-1 bg-gray-200 rounded w-full mx-auto my-2"></div>
+                        <div className="h-1 bg-gray-200 rounded w-full mx-auto my-2"></div>
 
-                        <div class="h-12 bg-gray-200 rounded w-full mx-auto"></div>
+                        <div className="h-12 bg-gray-200 rounded w-full mx-auto"></div>
 
-                        <div class="h-4 bg-gray-200 rounded w-full mx-auto mt-4"></div>
+                        <div className="h-4 bg-gray-200 rounded w-full mx-auto mt-4"></div>
                     </div>
                 </div>
             </div>
