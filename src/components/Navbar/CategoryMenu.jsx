9f9dda08cbb830cd036d0345b277b13563db5c29
Siam Khushnobish <siam@zaagsys.com>
2024-07-28 14:41:24 +0600

    update category menu for adding navigation

--- src/components/Navbar/CategoryMenu.jsx
+++ src/components/Navbar/CategoryMenu.jsx
@@ -27,12 +27,10 @@ const CategoryMenu = ({ onClose }) => {
             }  overflow-y-auto scrollbar-hide`}
         >
             <ul>
-                {categoriesData?.results?.map(
-                    (category, index) =>
-                        category?.subcategories?.length > 0 && (
-                            <li key={index} className="mb-2">
-                                <button
-                                    className={`
+                {categoriesData?.results?.map((category, index) => (
+                    <li key={index} className="mb-2">
+                        <button
+                            className={`
                 w-full flex  items-center text-xl
                 justify-between text-left font-medium py-4 px-4
                 ${
@@ -41,56 +39,59 @@ const CategoryMenu = ({ onClose }) => {
                         : ""
                 }
                 ${
-                    openCategory === category?.name
+                    openCategory === category?.id
                         ? "text-magenta-600"
                         : "text-neutral-800"
                 }`}
-                                    onClick={() =>
-                                        toggleCategory(category?.name)
-                                    }
-                                >
-                                    {category?.name}
-                                    <Image
-                                        src={Icons.right_arrow_gray}
-                                        alt="right arrow"
-                                        className={`w-6 h-6 duration-300 ${
-                                            openCategory === category?.name
-                                                ? "rotate-90"
-                                                : "rotate-0"
-                                        }`}
-                                    />
-                                </button>
-                                <div
-                                    className={`overflow-hidden text-neutral-800 text-base transition-max-height duration-500 ease-in-out ${
-                                        openCategory === category?.name
-                                            ? "max-h-40"
-                                            : "max-h-0"
+                            onClick={() =>
+                                category?.subcategories?.length > 0
+                                    ? toggleCategory(category?.id)
+                                    : handleNavigation()
+                            }
+                        >
+                            {category?.name}
+                            {category?.subcategories?.length > 0 && (
+                                <Image
+                                    src={Icons.right_arrow_gray}
+                                    alt="right arrow"
+                                    className={`w-6 h-6 duration-300 ${
+                                        openCategory === category?.id
+                                            ? "rotate-90"
+                                            : "rotate-0"
                                     }`}
-                                >
-                                    <ul className="pl-4">
-                                        {category?.subcategories.map(
-                                            (subcategory, subIndex) => (
-                                                <li
-                                                    onClick={handleNavigation}
-                                                    key={subIndex}
-                                                    className={`mb-1 py-2 cursor-pointer ${
-                                                        subIndex !==
-                                                        category?.subcategories
-                                                            ?.length -
-                                                            1
-                                                            ? "border-b border-neutral-20"
-                                                            : ""
-                                                    }`}
-                                                >
-                                                    {subcategory?.name}
-                                                </li>
-                                            )
-                                        )}
-                                    </ul>
-                                </div>
-                            </li>
-                        )
-                )}
+                                />
+                            )}
+                        </button>
+                        <div
+                            className={`overflow-hidden text-neutral-800 text-base transition-max-height duration-500 ease-in-out ${
+                                openCategory === category?.id
+                                    ? "max-h-40"
+                                    : "max-h-0"
+                            }`}
+                        >
+                            <ul className="pl-4">
+                                {category?.subcategories.map(
+                                    (subcategory, subIndex) => (
+                                        <li
+                                            onClick={handleNavigation}
+                                            key={subIndex}
+                                            className={`mb-1 py-2 cursor-pointer ${
+                                                subIndex !==
+                                                category?.subcategories
+                                                    ?.length -
+                                                    1
+                                                    ? "border-b border-neutral-20"
+                                                    : ""
+                                            }`}
+                                        >
+                                            {subcategory?.name}
+                                        </li>
+                                    )
+                                )}
+                            </ul>
+                        </div>
+                    </li>
+                ))}
             </ul>
         </div>
     );
