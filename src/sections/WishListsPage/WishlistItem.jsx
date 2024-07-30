529f3b27f7d2876be0e3313bb2d03933dfc0c5bf
Siam Khushnobish <siam@zaagsys.com>
2024-07-30 17:00:15 +0600

    wish list card responsive issue resolved

--- src/sections/WishListsPage/WishlistItem.jsx
+++ src/sections/WishListsPage/WishlistItem.jsx
@@ -9,18 +9,20 @@ const WishlistItem = ({ item }) => {
     const { deleteWishlistItem, loading } = useWishlist();
 
     return (
-        <div className="flex flex-col sm:flex-row items-center justify-between bg-white">
+        <div className="flex items-center justify-between bg-white p-4 rounded-lg shadow-md">
             <Spin fullscreen spinning={loading} />
-            <div className="flex items-center gap-4">
-                <Image
-                    src={product.images[0].image}
-                    alt={product.name}
-                    width={80}
-                    height={80}
-                    className="object-cover rounded-sm"
-                />
-                <div className="space-y-2">
-                    <h3 className="text-base font-medium text-black-1000 font-poppins">
+            <div className="flex items-center gap-4 flex-1">
+                <div className="flex-shrink-0">
+                    <Image
+                        src={product.images[0].image}
+                        alt={product.name}
+                        width={80}
+                        height={80}
+                        className="object-cover rounded-sm h-full"
+                    />
+                </div>
+                <div className="flex-1">
+                    <h3 className="text-base font-medium text-black-1000 font-poppins break-words">
                         {product.name}
                     </h3>
                     <p className="text-neutral-300 text-base font-medium">
@@ -28,7 +30,7 @@ const WishlistItem = ({ item }) => {
                     </p>
                 </div>
             </div>
-            <div className="flex flex-col items-center justify-center mt-4 sm:mt-0">
+            <div className="flex flex-col items-center justify-center space-y-2 ml-4">
                 <button className="p-2" onClick={() => deleteWishlistItem(id)}>
                     <Image
                         alt="delete icon"
@@ -39,7 +41,7 @@ const WishlistItem = ({ item }) => {
                 </button>
                 <button className="p-2">
                     <Image
-                        alt="delete icon"
+                        alt="add to cart icon"
                         src={Icons.add_to_Cart}
                         width={24}
                         height={24}
